55203cc238a357631aeeafcd0a610d346a2e9d06
SriLathaCodes <srilatha.srao@gmail.com>
2024-11-04 06:48:47 +0530

    Implement Dark Mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import theme from "./theme";
 
 createRoot(document.getElementById("root")!).render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </ChakraProvider>
 );
