cac3955ae153ccf6468482a1d6dd8433298e3646
SriLathaCodes <srilatha.srao@gmail.com>
2024-11-12 06:50:26 +0530

    Filter Games By Genre

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
 
 import useData from "./useData";
+import { Genre } from "./useGenre";
 
 export interface Platform
 {
@@ -16,5 +17,5 @@ export interface Game {
   metacritic:number;
 }
 
-const useGames=()=> useData<Game>("/games");
+const useGames=(selectedGenre:Genre|null)=> useData<Game>("/games",{params:{genres:selectedGenre?.id}},[selectedGenre?.id]);
 export default useGames;
